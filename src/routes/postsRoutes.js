94e538cc773304d3233f97fb989ff4ec9c991e04
Thaís Cristine Coelho <tcristine84@gmail.com>
2024-11-21 17:34:59 +0000

    Fazendo upload de imagem

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,10 +1,24 @@
 import express from "express";
-import {listarPosts, postarNovoPost} from "../controllers/postsControllers.js";
+import multer from "multer";
+import {listarPosts, postarNovoPost, uploadImagem} from "../controllers/postsControllers.js";
+
+//Essa parte é para instalar o multer no sistema windows
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({dest: "./uploads", storage}) //Se for linux ou mac só essa linha sem storage
 
 const routes = (app) => {    
     app.use(express.json()); //Permite que o servidor interprete requisições com o corpo no
     app.get("/posts", listarPosts); //Rota para buscas todos os posts
     app.post("/posts", postarNovoPost); //Rota para criar um post
+    app.post("/upload", upload.single("imagem"), uploadImagem);
 }
 
 export default routes;
