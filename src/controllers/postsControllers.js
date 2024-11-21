c062229689743e08704d5de6c72ede63471ef526
Thaís Cristine Coelho <tcristine84@gmail.com>
2024-11-21 16:41:52 +0000

    Criando um post para enviar para a base de dados

--- src/controllers/postsControllers.js
+++ src/controllers/postsControllers.js
@@ -1,6 +1,17 @@
-import { getTodosPosts } from "../models/postsModels.js";
+import {getTodosPosts, criarPost} from "../models/postsModels.js";
 
 export async function listarPosts(req, res) {
     const posts = await getTodosPosts(); //Chama a função
-    res.status(200).json(posts); //Envia resposta http com status 200
-}; 
+    res.status(200).json(posts); //Envia resposta http com status 200(OK)
+}
+ 
+export async function postarNovoPost(req, res) {
+    const novoPost = req.body;
+    try {
+        const postCriado = await criarPost(novoPost);
+        res.status(200).json(postCriado);
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro":"Falha na requisição"});
+    }
+}
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { listarPosts } from "../controllers/postsControllers.js";
+import {listarPosts, postarNovoPost} from "../controllers/postsControllers.js";
 
 const routes = (app) => {    
     app.use(express.json()); //Permite que o servidor interprete requisições com o corpo no
     app.get("/posts", listarPosts); //Rota para buscas todos os posts
+    app.post("/posts", postarNovoPost); //Rota para criar um post
 }
 
 export default routes;
