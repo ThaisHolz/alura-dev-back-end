fd9feaf5c618eaa1ac28ce6037d0697026f24add
Thaís Cristine Coelho <tcristine84@gmail.com>
2024-11-22 15:38:06 +0000

    Nota para juntas o back e o front

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,6 +1,12 @@
 import express from "express";
 import multer from "multer";
 import {listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost} from "../controllers/postsControllers.js";
+import cors from "cors";
+
+const corsOptions = {
+    origin: "http://localhost:8000",
+    optionsSuccessStatus: 200
+}
 
 //Essa parte é para instalar o multer no sistema windows
 const storage = multer.diskStorage({
@@ -16,6 +22,7 @@ const upload = multer({storage: storage}) //Se for linux ou mac só essa linha s
 
 const routes = (app) => {    
     app.use(express.json()); //Permite que o servidor interprete requisições com o corpo no
+    app.use(cors(corsOptions)); //Avisa as requisições de back-end na porta 8000
     app.get("/posts", listarPosts); //Rota para buscas todos os posts
     app.post("/posts", postarNovoPost); //Rota para criar um post
     app.post("/upload", upload.single("imagem"), uploadImagem);
